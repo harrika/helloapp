6dc560531659e01d753f8da4d3aa552b11a2a338
l <henry.kirya@gmail.com>
2019-07-01 22:07:21 +0300

    improving html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "hello from kiwatule, attention please, this is the first and only deployment from    Uganda today, so be Happy dont worry"
+    render html: "<h2>hello from kiwatule</h2> <br> Attention please, this is the first and only deployment <br> from Uganda today, so be Happy and dont worry"
   end
 
   def goodbye
